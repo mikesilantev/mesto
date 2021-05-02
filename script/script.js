ac32697ac31cd94eb833674e7d26939122a73197
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-02 16:33:58 +0300

    feat: add content

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 // profile
+let formElement = document.querySelector('.popup');
 let profileElement = document.querySelector('.profile');
+
 let editButton = profileElement.querySelector('.profile__editButton');
 let profileTitle = profileElement.querySelector('.profile__title');
 let profileJob = profileElement.querySelector('.profile__subtitle');
 
 // popup
-let formElement = document.querySelector('.popup');
 let exitButton = formElement.querySelector('.popup__exit-button');
 let nameInput = formElement.querySelector('.popup__name-input');
 let jobInput = formElement.querySelector('.popup__job-input');
-let saveButton = formElement.querySelector('.popup__save-button');
 
 // Открыть закрыть
 function editProfile(){
@@ -20,15 +20,13 @@ function editProfile(){
 function exitProfile() {
     formElement.classList.remove('popup_opened');
 }
-
-
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     formElement.classList.remove('popup_opened');
 }
+
 editButton.addEventListener('click', editProfile);
 exitButton.addEventListener('click', exitProfile);
 formElement.addEventListener('submit', formSubmitHandler); 
