5ad04522df5f500ae468ea0fde956102a9000833
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-02 14:59:10 +0300

    feat: js ready v1.0

--- script/script.js
+++ script/script.js
@@ -1,36 +1,34 @@
+// profile
 let profileElement = document.querySelector('.profile');
 let editButton = profileElement.querySelector('.profile__editButton');
+let profileTitle = profileElement.querySelector('.profile__title');
+let profileJob = profileElement.querySelector('.profile__subtitle');
+
+// popup
 let formElement = document.querySelector('.popup');
 let exitButton = formElement.querySelector('.popup__exit-button');
+let nameInput = formElement.querySelector('.popup__name-input');
+let jobInput = formElement.querySelector('.popup__job-input');
+let saveButton = formElement.querySelector('.popup__save-button');
 
-
+// Открыть закрыть
 function editProfile(){
     formElement.classList.add('popup_opened');
+    nameInput.setAttribute('value', profileTitle.textContent);
+    jobInput.setAttribute('value', profileJob.textContent);
 }
-
-editButton.addEventListener('click', editProfile);
-
 function exitProfile() {
     formElement.classList.remove('popup_opened');
 }
 
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault();
+    profileTitle.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    formElement.classList.remove('popup_opened');
+}
+editButton.addEventListener('click', editProfile);
 exitButton.addEventListener('click', exitProfile);
-
-
-
-// Находим поля формы в DOM
-let nameInput = formElement.querySelector('.popup__name-input');// Воспользуйтесь инструментом .querySelector()
-console.log(nameInput);
-let jobInput = formElement.querySelector('.popup__job-input');// Воспользуйтесь инструментом .querySelector()
-console.log(jobInput);
-
-
-
-// let exitButton = formElement.querySelector('.popup__exit-button');
-// console.log(exitButton);
-
-// function closePopup() {
-//     exitButton.classList.add('popup_opened');
-// }
-
-// exitButton.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler); 
