147b6b85fcf2f48408c663410ed114806880e9f6
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-10 21:04:59 +0300

    feat: js fix

--- script/script.js
+++ script/script.js
@@ -1,22 +1,22 @@
 // profile
 let formElement = document.querySelector('.popup');
 let profileElement = document.querySelector('.profile');
-
-// let editButton = profileElement.querySelector('.profile__editButton');
 let editButton = profileElement.querySelector('.profile__title');
 let profileTitle = profileElement.querySelector('.profile__title');
 let profileJob = profileElement.querySelector('.profile__subtitle');
-
+let avatarImg = document.querySelector('.profile__avatar');
+console.log(avatarImg);
 // popup
 let exitButton = formElement.querySelector('.popup__exit-button');
 let nameInput = formElement.querySelector('.popup__name-input');
 let jobInput = formElement.querySelector('.popup__job-input');
-
 // Открыть закрыть
 function editProfile(){
     formElement.classList.add('popup_opened');
     nameInput.setAttribute('value', profileTitle.textContent);
     jobInput.setAttribute('value', profileJob.textContent);
+    avatarImg.setAttribute('alt', profileJob.textContent);
+    avatarImg.setAttribute('title', profileTitle.textContent);
 }
 function exitProfile() {
     formElement.classList.remove('popup_opened');
@@ -30,4 +30,4 @@ function formSubmitHandler (evt) {
 
 editButton.addEventListener('click', editProfile);
 exitButton.addEventListener('click', exitProfile);
-formElement.addEventListener('submit', formSubmitHandler); 
+formElement.addEventListener('submit', formSubmitHandler);
