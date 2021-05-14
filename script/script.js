dc049b3c8660852302ce8e6e681229ddf6f1dced
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-14 11:56:49 +0300

    ready

--- script/script.js
+++ script/script.js
@@ -1,33 +1,26 @@
 let formElement = document.querySelector('.popup__form');
 let profileElement = document.querySelector('.profile');
 let overleyPopup = document.querySelector('.overlay');
-
-//profile
 let profileTitle = profileElement.querySelector('.profile__title');
 let profileJob = profileElement.querySelector('.profile__subtitle');
 let editButton = profileElement.querySelector('.profile__edit-button');
-
 let exitButton = overleyPopup.querySelector('.popup__exit-button'); 
-let nameInput = formElement.querySelector('.popup__input_name'); 
-let jobInput = formElement.querySelector('.popup__input_job'); 
-//open
+let nameInput = formElement.querySelector('.popup__input_text_name'); 
+let jobInput = formElement.querySelector('.popup__input_text_job'); 
 function popupOpen(){
   overleyPopup.classList.add('popup_opened');
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileJob.textContent;
 }
-// close
 function popupClose(){
   overleyPopup.classList.remove('popup_opened');
 }
-//formSubmitHandler
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileTitle.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     popupClose();
 }
-//Listener
 editButton.addEventListener('click', popupOpen);
 exitButton.addEventListener('click', popupClose);
 formElement.addEventListener('submit', formSubmitHandler);
