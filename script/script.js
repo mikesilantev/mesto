faadc513bd1b5c2b65fc93734cde62bd4f18a0b7
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-18 16:53:17 +0300

    js like ready

--- script/script.js
+++ script/script.js
@@ -7,6 +7,54 @@ let editButton = profileElement.querySelector('.profile__edit-button');
 let exitButton = overleyPopup.querySelector('.popup__exit-button'); 
 let nameInput = formElement.querySelector('.popup__input_text_name'); 
 let jobInput = formElement.querySelector('.popup__input_text_job'); 
+//like
+for (let like of document.querySelectorAll('.element__like')){
+  like.addEventListener('click', function(){
+    this.classList.toggle('element__like_active');
+  })
+}
+
+// const cardElement = document.querySelector('.element');
+// const likeElement = cardElement.querySelector('.element__like');
+// function clickLike (){
+//   likeElement.addEventListener('click', function (evt) {
+//         const eventTarget = evt.target;
+//         console.log(eventTarget);
+//         evt.target.classList.toggle('element__like_active');
+//   });
+// }
+
+// cardElement.addEventListener('click', clickLike);
+
+
+// const cardElement = document.querySelector('.element');
+// console.log(cardElement);
+
+// const likeElement = cardElement.querySelector('.element__like');
+// console.log(likeElement);
+
+// likeElement.addEventListener('click', function (evt) {
+//       const eventTarget = evt.target;
+//       console.log(eventTarget);
+//       evt.target.classList.toggle('_active');
+// });
+
+
+
+
+
+// function clickLike() {
+//   const cardElement = document.querySelector('.element');
+//   console.log(cardElement);
+//   const likeElement = document.querySelector('.element__like');
+//   console.log(likeElement);
+//   likeElement.addEventListener('click', function (evt) {
+//     console.log(evt);
+//     // evt.target.classList.add('_active');
+//   });
+// }
+// clickLike();
+
 function openPopup(){
   overleyPopup.classList.add('overlay_popup-open');
   nameInput.value = profileTitle.textContent;
