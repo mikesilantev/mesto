c1436e8cc00eff25f8c1f78ff6492a58eee640ae
Mike Silantev <mike.amigofish@yandex.ru>
2021-05-28 11:44:44 +0300

    fix/hotfix-3

--- script/script.js
+++ script/script.js
@@ -1,135 +1,154 @@
+//Mesto 1.0.3 
+//28.05.2021
+
+//Array with data
 const initialCards = [
   {
-    name: 'Marrakesh, Morocco',
-    link: 'https://images.unsplash.com/photo-1528499640293-fe8fba1f28c4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+    name: 'GAME OVER',
+    link: 'https://images.unsplash.com/photo-1579373903781-fd5c0c30c4cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
   },
   {
-    name: 'República, Brazil',
-    link: 'https://images.unsplash.com/photo-1522036440821-525d53504f74?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2135&q=80'
+    name: '30 years of the Nintendo Game Boy.',
+    link: 'https://images.unsplash.com/photo-1555864326-5cf22ef123cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1047&q=80'
   },
   {
-    name: 'Vienna, Austria',
-    link: 'https://images.unsplash.com/photo-1507808973436-a4ed7b5e87c9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80'
+    name: 'Super Nintendo Entertainment System - controller',
+    link: 'https://images.unsplash.com/photo-1531594896955-305cf81269f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
   },
   {
-    name: 'Blue Mountains, Australia',
-    link: 'https://images.unsplash.com/photo-1542605731-cf0169ad50e3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1906&q=80'
+    name: 'NES - Mini with Mario Kart (FrontView)',
+    link: 'https://images.unsplash.com/photo-1591462391994-cb7614f54aff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
   },
   {
-    name: 'Havana is well know for its classic old American cars.',
-    link: 'https://images.unsplash.com/photo-1530568595540-2b7554622f92?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1952&q=80'
+    name: 'Arcade',
+    link: 'https://images.unsplash.com/photo-1583634852966-130c15654305?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=665&q=80'
   },
   {
-    name: 'Demons Dance, Barcelona',
-    link: 'https://images.unsplash.com/photo-1563271834-46ce38124976?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1341&q=80'
+    name: 'Nintendo 2ds',
+    link: 'https://images.unsplash.com/photo-1593693846852-afc7461355ad?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA2fHxuaW50ZW5kbyUyMGdhbWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
   },
 ];
+//Modal
 const modalEdit = document.querySelector('.popup_type_edit');
+//Modal Add Card
 const modalAdd = document.querySelector('.popup_type_add');
+const locInput = modalAdd.querySelector('.popup__input_text_loc-name');
+const urlInput = modalAdd.querySelector('.popup__input_text_url');
+// Modal Image
 const modalImage = document.querySelector('.popup_type_image');
+const modalImageFull = modalImage.querySelector('.popup__image');
+const modalImageSubtitle = modalImage.querySelector('.popup__subtitle');
+//Card <template> Element
 const cardTemplate = document.querySelector('#card-template').content;
-// console.log(cardTemplate);
-//Секция элементс для вставки карточек
 const cardsContainer = document.querySelector('.elements');
-//Создание разметки для карточки//////////////////////////////////////
-function createCardDOM() {
-  // console.log('Создаем разметку')
+//Create card DOM
+
+
+function createCardDOM(name, url) {
+  console.log('Создаем разметку');
   const cardElement = cardTemplate.querySelector('.element').cloneNode(true);
+  const cardImageRender = cardElement.querySelector('.element__image');
+  const cardSubtitileElement = cardElement.querySelector('.element__title');
+  //like
+  const cardElementLike = cardElement.querySelector('.element__like');
+
+  //Set attribute element
+  cardImageRender.src = url;
+  cardImageRender.alt = name;
+  cardSubtitileElement.textContent = name;
+
+  // listener and attribute for the modal image from the function values (name & url)
   cardElement.querySelector('.element__image').addEventListener('click', () => {
-    console.log(cardElement.parentNode);
+    modalImageFull.src = url;
+    modalImageFull.alt = name;
+    modalImageSubtitle.textContent = name;
+    // console.log(cardElement);
     openModal(modalImage);
   })
 
+  //Trash button - listener & remove card
   cardElement.querySelector('.element__trash').addEventListener('click', (evt) => {
-    evt.target.closest(".element").remove();
+    evt.target.closest('.element').remove();
   })
-  const cardElementLike = cardElement.querySelector('.element__like');
+
+  //Like button - listener & toggle class
   cardElementLike.addEventListener('click', () => {
     cardElementLike.classList.toggle('element__like_active')
   })
   return cardElement;
 }
-//Добавляем карточку к контейнер
-const modalImageSrc = modalImage.querySelector('.popup__image');
-const modalImageAlt = modalImage.querySelector('.popup__image');
-const modalImageSubtitle =modalImage.querySelector('.popup__subtitle');
 
-function renderCard(name, url) {
-  let newCard = createCardDOM();
-  let cardImageRender = newCard.querySelector('.element__image');
-  let cardSubtitleElement = newCard.querySelector('.element__title');
-  cardImageRender.src = url;
-  cardSubtitleElement.textContent = name;
-  modalImageSrc.src = url;
-  modalImageAlt.alt = name;
-  modalImageSubtitle.textContent = name;
+//Add card to section elements
+function renderCard(newCard) {
   cardsContainer.prepend(newCard);
 }
-//Перебираем массив и создаем DOM для карточек
+
+//Iterate through the array, create a DOM, and add it to the section
 initialCards.forEach((array) => {
-  let arrayName = array.name;
-  let arrayLink = array.link;
-  renderCard(arrayName, arrayLink);
+  const newCard = createCardDOM(array.name, array.link);
+  renderCard(newCard);
 })
-//Добавление новой карточки
+
+//Add new card - conditions, create DOM & add it to the section
 function addCard(evt) {
   evt.preventDefault();
-  const locInput = modalAdd.querySelector('.popup__input_text_loc-name');
-  const urlInput = modalAdd.querySelector('.popup__input_text_url');
   if ((locInput.value === '') || (urlInput.value === '')) {
     closeModal(modalAdd);
   }
   if ((locInput.value != '') || (urlInput.value != '')) {
-    createCardDOM();
-    renderCard(locInput.value, urlInput.value);
+    const newCard = createCardDOM(locInput.value, urlInput.value);
+    renderCard(newCard);
     closeModal(modalAdd);
+    //return blank input
+    locInput.value = '';
+    urlInput.value = '';
   }
 }
+//Button - edit profile
 const buttonEdit = document.querySelector('.profile__edit-button');
 buttonEdit.addEventListener('click', () => {
-  // console.log('Кликнули по кнопке редактирования')
   openModal(modalEdit);
   pickProfileInfo();
 })
-//Кнопка добавления новых карточек
+//Button - add new card 
 const buttonAdd = document.querySelector('.profile__add-button');
 buttonAdd.addEventListener('click', () => {
-  // console.log('Кликнули по кнопке добавления картинок')
   openModal(modalAdd);
 })
+//Open all modal =*) (Khaz Modan)
 function openModal(modal) {
   modal.classList.toggle('popup_open');
 }
-//Листенер на закрытие окна редактирования
+//Listener on close button for Modal Edit Profile
 modalEdit.querySelector('.popup__exit-button').addEventListener('click', () => {
   closeModal(modalEdit);
 })
-//Листенер на закрытие окна добавления
+//Listener on close button for Modal Add New Card
 modalAdd.querySelector('.popup__exit-button').addEventListener('click', () => {
   closeModal(modalAdd);
 })
-//Листенер на закрытие модалки с картинкой
+//Listener on close button for Modal Image
 modalImage.querySelector('.popup__exit-button').addEventListener('click', () => {
   closeModal(modalImage);
 })
+//Close all modal
 function closeModal(modal) {
   modal.classList.remove('popup_open')
 }
-//Забираем данные из профиля
-// Переменные
+// get data in profile
 const profileElement = document.querySelector('.profile');
 const nameInput = document.querySelector('.popup__input_text_name');
 const jobInput = document.querySelector('.popup__input_text_job');
 const profileTitle = profileElement.querySelector('.profile__title');
 const profileJob = profileElement.querySelector('.profile__subtitle');
-// Вставка инфы из профиля в попап
 function pickProfileInfo() {
   nameInput.value = profileTitle.textContent;
   jobInput.value = profileJob.textContent;
 }
+//listener on submit butoon
 modalEdit.addEventListener('submit', edirProfile);
 modalAdd.addEventListener('submit', addCard);
-
+//Edit profile
 function edirProfile(evt) {
   evt.preventDefault();
   profileTitle.textContent = nameInput.value;
