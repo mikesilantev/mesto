8a0609ea547f1f277c6760ae92124a13c67f8fa2
Mike Silantev <mike.amigofish@yandex.ru>
2021-06-20 16:58:21 +0300

    cross

---
+++ script/initial-сards.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'GAME OVER',
+    link: 'https://images.unsplash.com/photo-1579373903781-fd5c0c30c4cd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=967&q=80'
+  },
+  {
+    name: '30 years of the Nintendo Game Boy.',
+    link: 'https://images.unsplash.com/photo-1555864326-5cf22ef123cf?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1047&q=80'
+  },
+  {
+    name: 'Super Nintendo Entertainment System - controller',
+    link: 'https://images.unsplash.com/photo-1531594896955-305cf81269f1?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  },
+  {
+    name: 'NES - Mini with Mario Kart (FrontView)',
+    link: 'https://images.unsplash.com/photo-1591462391994-cb7614f54aff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80'
+  },
+  {
+    name: 'Arcade',
+    link: 'https://images.unsplash.com/photo-1583634852966-130c15654305?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=665&q=80'
+  },
+  {
+    name: 'Nintendo 2ds',
+    link: 'https://images.unsplash.com/photo-1593693846852-afc7461355ad?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTA2fHxuaW50ZW5kbyUyMGdhbWV8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
+  },
+];
